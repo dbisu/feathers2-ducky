1396c103ca3828c7e60f342ff07cb0f5ce136abd
dbisu <dabailey@gmail.com>
2021-12-06 22:40:52 -0600

    Initial file setup.  Copied from pico-ducky with changes for FeatherS2 board

---
+++ boot.py
@@ -0,0 +1,21 @@
+# License : GPLv2.0
+# copyright (c) 2021  Dave Bailey
+# Author: Dave Bailey (dbisu, @daveisu)
+
+
+from board import *
+import digitalio
+import storage
+
+noStorageStatus = False
+noStoragePin = digitalio.DigitalInOut(IO35)
+noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
+noStorageStatus = not noStoragePin.value
+
+if(noStorageStatus == True):
+    # don't show USB drive to host PC
+    storage.disable_usb_drive()
+    print("Disabling USB drive")
+else:
+    # normal boot
+    print("USB drive enabled")
